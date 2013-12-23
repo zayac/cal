4f92e5493f6a1be66849c4da899232406f74e49d
Pavel Zaichenkov <zaichenkov@gmail.com>
2013-12-24 01:45:31 +0400

    Some functions from solver module are moved from external to internal

--- src/solver.mli
+++ src/solver.mli
@@ -21,24 +21,6 @@ type var_constr =
     constraints. *)
 exception Unsatisfiability_Error of string
 
-(** [add_list_cstr_exn cm v] adds a new constraint to map of constraints [cm]
-    setting variable [v] to a list term. Throws [Unsatisfiability_Error] if a
-    conflicting constraint is found. *)
-val add_list_cstr_exn : var_constr String.Map.t -> string ->
-      var_constr String.Map.t
-
-(** [add_record_cstr_exn cm v] adds a new constraint to map of constraints [cm]
-    setting variable [v] to a record term. Throws [Unsatisfiability_Error] if a
-    conflicting constraint is found. *)
-val add_record_cstr_exn : var_constr String.Map.t -> string -> String.Set.t ->
-      var_constr String.Map.t
-
-(** [add_choice_cstr_exn cm v] adds a new constraint to map of constraints [cm]
-    setting variable [v] to a choice term. Throws [Unsatisfiability_Error] if a
-    conflicting constraint is found. *)
-val add_choice_cstr_exn : var_constr String.Map.t -> string -> String.Set.t ->
-      var_constr String.Map.t
-
 (** A unification procedure that finally returns unified constraints on
     variables. *)
 val unify_exn : Network.G.t -> var_constr String.Map.t
